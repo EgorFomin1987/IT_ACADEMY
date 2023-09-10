5325cceda51122c8daf10cb7057219ae26fd0dac
Egor Fomin <egorfomin1987@gmail.com>
2023-09-10 15:52:10 +0300

    homework-2 changes

--- homework-2/homework-2-7/src/Main.java
+++ homework-2/homework-2-7/src/Main.java
@@ -3,8 +3,7 @@ public class Main {
         var a = 6;
         var b = 8;
         var r = 5;
-        double Radius = Math.sqrt((double)(Math.pow(a,2) + Math.pow(b,2)));
-        System.out.println(r >= Radius);
+        double Diametr = Math.sqrt((double)(Math.pow(a,2) + Math.pow(b,2)));
+        System.out.println(r >= Diametr);
     }
 }
-
